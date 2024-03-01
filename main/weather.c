fb22aad2015905a191ad674b71aaf4608c6289d0
ohmybabydoll <331597245@qq.com>
2024-03-01 13:22:24 +0800

    first commit

---
+++ main/weather.c
@@ -0,0 +1,217 @@
+#include <string.h>
+#include <sys/param.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "protocol_examples_utils.h"
+#include "esp_tls.h"
+#if CONFIG_MBEDTLS_CERTIFICATE_BUNDLE
+#include "esp_crt_bundle.h"
+#endif
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+
+#include "esp_http_client.h"
+#include "cjson.h"   // json 解析
+#include "weather.h"
+#include "driver/gpio.h"
+
+
+
+static const char *TAG = "HTTP_CLIENT"; //用于打日志
+
+static int temp = 0;  //温度
+static char *name;   //城市名称
+static char *text;   // 天气
+static char *wind_class;  //风力
+static char *wind_dir;   //风向
+static int rh=0;   //湿度
+
+
+void app_main(void)
+{   esp_err_t ret = initnvs();
+    ESP_ERROR_CHECK(ret);  
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+    ESP_LOGI(TAG,"Connected to AP, begin http example");
+
+    xTaskCreate(&http_test_task, "http_test_task", 8192, NULL, 5, NULL); // 创建一个task任务，类似于线程。
+}
+
+/**
+ * 初始化 nvs（非易失性存储）断电后可保存数据，通常用来保存配置数据。
+*/
+static esp_err_t initnvs(void){
+    esp_err_t ret = nvs_flash_init();   //初始化
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase()); // 若不可用，擦除数据重新初始化
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);  // 若有错误终止程序
+    return ret;
+}
+
+/**
+ * task 回调函数  按键调用 http_rest_with_url()
+*/
+static void http_test_task(void *pvParameters) {
+  while(1){
+    if(gpio_get_level(GPIO_INPUT_PIN)==0) {
+      http_rest_with_url();
+      vTaskDelay(pdMS_TO_TICKS(100));  // 延时100ms消除抖动
+    }
+    vTaskDelay(pdMS_TO_TICKS(100));     
+  }
+  vTaskDelete(NULL);
+}
+
+static void http_rest_with_url(void)
+{
+    // 缓冲区用于存储请求响应数据
+    // Declare local_response_buffer with size (MAX_HTTP_OUTPUT_BUFFER + 1) to prevent out of bound access when
+    // it is used by functions like strlen(). The buffer should only be used upto size MAX_HTTP_OUTPUT_BUFFER
+    char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER + 1] = {0};
+    /**
+     * NOTE: All the configuration parameters for http_client must be spefied either in URL or as host and path parameters.
+     * If host and path parameters are not set, query parameter will be ignored. In such cases,
+     * query parameter should be specified in URL.
+     *
+     * If URL as well as host and path parameters are specified, values of host and path will be considered.
+     */
+    esp_http_client_config_t config = {
+        .url="http://api.map.baidu.com/weather/v1/?district_id=510185&data_type=all&ak=7hl2vs5iRhLYzdfDb7uKRi88s75mZ4Xb",
+        .event_handler = _http_event_handler,
+        .user_data = local_response_buffer,        // Pass address of local buffer to get response
+        .disable_auto_redirect = true,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    // GET
+    esp_err_t err = esp_http_client_perform(client);
+    ESP_LOGI(TAG, "HTTP GET Status = %d",err);
+
+    cJSON *root = cJSON_Parse(local_response_buffer);
+    cJSON *result = cJSON_GetObjectItem(root,"result");
+    cJSON *location = cJSON_GetObjectItem(result,"location");
+    cJSON *now = cJSON_GetObjectItem(result,"now");
+    name = cJSON_GetObjectItem(location, "name")->valuestring;
+    text = cJSON_GetObjectItem(now, "text")->valuestring;
+    temp = cJSON_GetObjectItem(now, "temp")->valueint;
+    rh = cJSON_GetObjectItem(now, "rh")->valueint;
+    wind_class = cJSON_GetObjectItem(now, "wind_class")->valuestring;
+    wind_dir = cJSON_GetObjectItem(now, "wind_dir")->valuestring;
+
+    ESP_LOGI(TAG,"城市 %s",name);
+    ESP_LOGI(TAG,"天气 %s",text);
+    ESP_LOGI(TAG,"温度 %d",temp);
+    ESP_LOGI(TAG,"湿度 %d",rh);
+    ESP_LOGI(TAG,"风力 %s",wind_class);
+    ESP_LOGI(TAG,"风向 %s",wind_dir);
+
+    cJSON_Delete(root);
+    esp_http_client_cleanup(client);
+    
+}
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    static char *output_buffer;  // Buffer to store response of http request from event handler
+    static int output_len;       // Stores number of bytes read
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            // Clean the buffer in case of a new request
+            if (output_len == 0 && evt->user_data) {
+                // we are just starting to copy the output data into the use
+                memset(evt->user_data, 0, MAX_HTTP_OUTPUT_BUFFER);
+            }
+            /*
+             *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
+             *  However, event handler can also be used in case chunked encoding is used.
+             */
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                // If user_data buffer is configured, copy the response into the buffer
+                int copy_len = 0;
+                if (evt->user_data) {
+                    // The last byte in evt->user_data is kept for the NULL character in case of out-of-bound access.
+                    copy_len = MIN(evt->data_len, (MAX_HTTP_OUTPUT_BUFFER - output_len));
+                    if (copy_len) {
+                        memcpy(evt->user_data + output_len, evt->data, copy_len);
+                    }
+                } else {
+                    int content_len = esp_http_client_get_content_length(evt->client);
+                    if (output_buffer == NULL) {
+                        // We initialize output_buffer with 0 because it is used by strlen() and similar functions therefore should be null terminated.
+                        output_buffer = (char *) calloc(content_len + 1, sizeof(char));
+                        output_len = 0;
+                        if (output_buffer == NULL) {
+                            ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+                            return ESP_FAIL;
+                        }
+                    }
+                    copy_len = MIN(evt->data_len, (content_len - output_len));
+                    if (copy_len) {
+                        memcpy(output_buffer + output_len, evt->data, copy_len);
+                    }
+                }
+                output_len += copy_len;
+            }
+
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+            if (output_buffer != NULL) {
+                // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+                // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            int mbedtls_err = 0;
+            esp_err_t err = esp_tls_get_and_clear_last_error((esp_tls_error_handle_t)evt->data, &mbedtls_err, NULL);
+            if (err != 0) {
+                ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
+                ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+            }
+            if (output_buffer != NULL) {
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+        case HTTP_EVENT_REDIRECT:
+            ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
+            esp_http_client_set_header(evt->client, "From", "user@example.com");
+            esp_http_client_set_header(evt->client, "Accept", "text/html");
+            esp_http_client_set_redirection(evt->client);
+            break;
+    }
+    return ESP_OK;
+}
---
+++ main/weather.h
@@ -0,0 +1,28 @@
+#ifndef _WEATHER_H
+#define _WEATHER_H
+
+#include "esp_err.h"
+#include "esp_http_client.h"
+#include "driver/gpio.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MAX_HTTP_OUTPUT_BUFFER 2048
+#define GPIO_INPUT_PIN GPIO_NUM_0
+
+static esp_err_t initnvs(void);
+
+static void http_test_task(void *pvParameters);
+
+static void http_rest_with_url(void);
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
